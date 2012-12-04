7fac660f731b7897530f2562d46f37b671442eae
quoc.thai.phan@gmail.com <quoc.thai.phan@gmail.com@b4e93495-4163-cf53-e6ed-290ce8afcf24>
2012-12-04 23:12:42 +0000

    Version 1

---
+++ Transceo Andoid App/src/com/transceo/app/android/MainActivity.java
@@ -0,0 +1,33 @@
+package com.transceo.app.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	/** Called when the user clicks the Send button */
+	public void sendMessage(View view) {
+		// Intent intent = new Intent(this, BookingActivity.class);
+		// EditText mailText = (EditText) findViewById(R.id.edit_mail);
+		// EditText passwordText = (EditText) findViewById(R.id.edit_password);
+		// String mail = mailText.getText().toString();
+		// String password = passwordText.getText().toString();
+		// intent.putExtra(EXTRA_MESSAGE, mail);
+
+		// startActivity(intent);
+	}
+}
